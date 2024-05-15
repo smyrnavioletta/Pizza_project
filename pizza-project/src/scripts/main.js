11b4f5ee86cec9ac9c6264bcad542e72de334af8
Violetta <dylisa.shoes@gmail.com>
2024-05-15 18:56:05 +0300

    add some changes

--- pizza-project/src/scripts/main.js
+++ pizza-project/src/scripts/main.js
@@ -129,13 +129,3 @@ document.addEventListener('DOMContentLoaded', () => {
     basket.render('.basket');
 });
 
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-});
